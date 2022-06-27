5ed984ab5fc0c8eb30dcc568c123e91b7f52788a
Bogdan Cuza <bogdancuza@protonmail.com>
2022-06-27 20:20:37 +0300

    commit tree 30b1d6c48de7200615391cd320dcb8c6d485d394

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,22 @@
 class Song < ActiveRecord::Base
   # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
+  def artist_name
+    artist.name unless artist.nil?
+  end
+
+  def artist_name=(name)
+    Artist.find_or_create_by(name: name).songs << self
+  end
+
+  def note_contents
+    notes.map {|n| n.content}
+  end
+
+  def note_contents=(contents)
+    contents.each {|content| self.notes.build(content: content) unless content == ""}
+  end
 end
